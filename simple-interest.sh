15b255176cf7387723e6d4534a8b99be5909f222
shaswat128 <shaswatpanda128@gmail.com>
2026-03-21 11:42:50 +0530

    Add simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
 
+# Simple Interest Calculator
+
+echo "Simple Interest Calculator"
+echo "---------------------------"
+
+# Take user input
+read -p "Enter Principal amount: " principal
+read -p "Enter Rate of Interest (%): " rate
+read -p "Enter Time (in years): " time
+
+# Calculate Simple Interest
+simple_interest=$(echo "scale=2; ($principal * $rate * $time) / 100" | bc)
+
+# Display result
+echo "--------------------------------"
+echo "Simple Interest = $simple_interest"
